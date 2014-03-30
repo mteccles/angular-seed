87cd765c3bc464a3bdd1397d25bd49b4b2821100
d345768 <mark.eccles@team.telstra.com>
2014-03-30 22:18:28 +1100

    initial changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['myApp.services'])
+    .controller('MyCtrl1', ['$scope', 'Recipe', function ($scope, Recipe) {
+        Recipe().then(function (response) {
+            $scope.recipe = response.data;
+        });
 
-  }])
-  .controller('MyCtrl2', [function() {
 
-  }]);
+    }])
+    .controller('MyCtrl2', [function () {
+
+    }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,12 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var services = angular.module('myApp.services', []);
+  services.value('version', '0.1');
+
+services.factory('Recipe', ['$http', function($http) {
+    return function() {
+        return $http.get("http://localhost:3000/wines/1");
+        //return ['a','b','c'];
+    }
+}]);
